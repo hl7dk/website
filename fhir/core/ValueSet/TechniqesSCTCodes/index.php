d3f0eab82accefba9e3e49a948501c0f06544a93
Jens Kristian Villadsen <jenskristianvilladsen@gmail.com>
2023-12-01 14:44:33 +0100

    Added release 3.0.0

--- fhir/core/ValueSet/TechniqesSCTCodes/index.php
+++ fhir/core/ValueSet/TechniqesSCTCodes/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('http://hl7.dk/fhir/core/2.2.0/ValueSet-TechniqesSCTCodes.json2');
+  Redirect('http://hl7.dk/fhir/core/3.0.0/ValueSet-TechniqesSCTCodes.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('http://hl7.dk/fhir/core/2.2.0/ValueSet-TechniqesSCTCodes.json1');
+  Redirect('http://hl7.dk/fhir/core/3.0.0/ValueSet-TechniqesSCTCodes.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('http://hl7.dk/fhir/core/2.2.0/ValueSet-TechniqesSCTCodes.json');
+  Redirect('http://hl7.dk/fhir/core/3.0.0/ValueSet-TechniqesSCTCodes.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('http://hl7.dk/fhir/core/2.2.0/ValueSet-TechniqesSCTCodes.xml2');
+  Redirect('http://hl7.dk/fhir/core/3.0.0/ValueSet-TechniqesSCTCodes.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('http://hl7.dk/fhir/core/2.2.0/ValueSet-TechniqesSCTCodes.xml1');
+  Redirect('http://hl7.dk/fhir/core/3.0.0/ValueSet-TechniqesSCTCodes.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('http://hl7.dk/fhir/core/2.2.0/ValueSet-TechniqesSCTCodes.html');
+  Redirect('http://hl7.dk/fhir/core/3.0.0/ValueSet-TechniqesSCTCodes.html');
 else 
-  Redirect('http://hl7.dk/fhir/core/2.2.0/ValueSet-TechniqesSCTCodes.xml');
+  Redirect('http://hl7.dk/fhir/core/3.0.0/ValueSet-TechniqesSCTCodes.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.
